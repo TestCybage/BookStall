442d3fe510a8ff5922f430e19f91595c20919176
aashaykadu <aashayk@cybage.com>
2022-12-28 21:34:30 +0530

    front end started

--- FrontEnd/react-client/src/App.js
+++ FrontEnd/react-client/src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { ToastContainer } from "react-toastify";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { AllBooks } from './components/admin/AllBooks';
 
 function App() {
   return (
     <div className="App"> 
-    
+    {/* <BrowserRouter>
+        <Routes>
+        <Route path="/getAllBooks" element={<AllBooks/>} />
+
+
+        </Routes>
+    </BrowserRouter> */}
+    <AllBooks/>
+    <ToastContainer theme="colored" />
     </div>
   );
 }
